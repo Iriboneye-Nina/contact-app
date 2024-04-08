8ade1cf18f0ee74d05e99b2557cbf86ab5ae4cb9
Brice Kelly <ntirukelly@gmail.com>
2024-04-08 22:29:39 +0200

    other-commit

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -3,46 +3,48 @@ import React from 'react'
 function Body() {
   return (
     <>
-     <div className='p-10'>
-     <div className='flex justify-center gap-80 px-10'>
-      <span className='font-bold size-medium'>Welcome to contact section </span>
-      <button id='btn' className='border-r text-center bg-black text-white rounded-md w-20 h-12'>Add New</button>
-      </div>
-    </div>
-    <div id='container' className='flex justify-center gap-80 px-10 bg-slate-400 w-6/6 h-12 md:20%'>
-      <div>
-        Uwera Anick<br></br>
-        0726548043
-      </div>
-      <div>
-        ...<br></br>
-        anickyuwera@gmail.com
-      </div>
-    </div>
-    <div class="card bg-slate-200">
-    <div class='flex justify-center p-10 gap-16'>
-        <div>
-            Uwera Anick<br></br>
-            0726548043
-        </div>
-        <div>
-            ...<br></br>
-            anickyuwera@gmail.com
-        </div>
-    </div>
+<div className="p-10">
+  <div className="flex justify-center gap-80 px-10">
+    <span className="font-bold text-medium">Welcome to the contact section</span>
+    <button id="btn" className="border-r text-center bg-black text-white rounded-md w-20 h-12">Add New</button>
+  </div>
 </div>
 
-<div class="card bg-slate-500">
-    <div class='flex justify-center p-6 gap-16'>
-        <div>
-            Uwera Anick<br></br>
-            0726548043
-        </div>
-        <div>
-            ...<br></br>
-            anickyuwera@gmail.com
-        </div>
+<div id="container" className="flex justify-center gap-80 px-10 space-x-8 bg-slate-400 p-6">
+  <div>
+    Uwera Anick<br></br>
+    0726548043
+  </div>
+  <div>
+    ...<br></br>
+    anickyuwera@gmail.com
+  </div>
+</div>
+
+<div className="card bg-slate-400 my-4">
+  <div className="flex justify-center p-6 gap-16">
+    <div>
+      Uwera Anick<br></br>
+      0726548043
     </div>
+    <div>
+      ...<br></br>
+      anickyuwera@gmail.com
+    </div>
+  </div>
+</div>
+
+<div className="card bg-slate-400 my-4">
+  <div className="flex justify-center p-6 gap-16">
+    <div>
+      Uwera Anick<br></br>
+      0726548043
+    </div>
+    <div>
+      ...<br></br>
+      anickyuwera@gmail.com
+    </div>
+  </div>
 </div>
 
     </>
